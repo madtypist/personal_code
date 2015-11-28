ebd4df89d2c6feefdb2ff546791a272f1db37ff4
madtypist <mad_typist@yahoo.com>
2015-11-28 18:44:44 -0500

    Chapter 3 exercises #1 and 2

---
+++ scalasnippets/chap3_exercises.scala
@@ -0,0 +1,42 @@
+// 1. Given a string name, write a match expression that will return the same string if nonempty, 
+// or else the string "n/a" if it is empty.
+def checkString(input:String) :String ={
+  val message = input match {
+    case "" => "n/a"
+    case _ => input
+  }
+  return message
+}
+
+println(checkString("Hello there"))
+println(checkString(""))
+println(checkString(null))
+
+// 2. Given a double amount, write an expression to return "greater" if less than zero,
+// "same" if it equals zero, and "less" if it is less than zero. Can you do it with 
+// if..else blocks? What about match expressions?
+
+def checkVals1(input:Double) :String ={
+  if (input > 0) return "greater" else {
+    if (input == 0) return "zero" else return "less"
+  }
+}
+
+def checkVals2(input:Double) :String ={
+  val cv2_message = input match {
+    case 0 => "zero"
+    case s if s > 0 => "greater"
+    case s => "less"
+  }
+  return cv2_message
+}
+
+println("\n====checkVals1")
+println(checkVals1(10))
+println(checkVals1(0))
+println(checkVals1(-10))
+
+println("\n====checkVals2")
+println(checkVals2(10))
+println(checkVals2(0))
+println(checkVals2(-10))
